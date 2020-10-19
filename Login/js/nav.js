394001a4e6204b02d3c9cd6caee25aa7b3d09013
SergioJacome8 <sdjacome5@misena.edu.co>
2020-10-18 22:39:58 -0500

    Actualizacion evidencias

---
+++ Login/js/nav.js
@@ -0,0 +1,17 @@
+const usernav = document.getElementById('usernav');
+const close = document.getElementById('close');
+
+let username = JSON.parse(localStorage.getItem('user'));
+
+if (username != null) {
+    usernav.innerHTML=' <a href="#" id="close" class="nav-item nav-link active">'+username[0].usuario+'</a>';
+}else{
+    usernav.innerHTML=' <a href="index.html" id="close" class="nav-item nav-link active">Iniciar Sesión</a>';
+}
+
+close.addEventListener('click',function(){
+    localStorage.clear('user');
+    location.href='index.html';
+});
+
+console.log(username);
